a28fd1eb9d8f87e9af6667cd99d3c5c98c7d829d
lucky_block <1774432258@qq.com>
2025-12-09 09:51:57 +0800

    规范kjs兼容写法

--- src/main/java/com/mo_guang/ctpp/CTPPGTAddon.java
+++ src/main/java/com/mo_guang/ctpp/CTPPGTAddon.java
@@ -2,12 +2,18 @@ package com.mo_guang.ctpp;
 
 import com.gregtechceu.gtceu.api.addon.GTAddon;
 import com.gregtechceu.gtceu.api.addon.IGTAddon;
+import com.gregtechceu.gtceu.api.addon.events.KJSRecipeKeyEvent;
 import com.gregtechceu.gtceu.api.registry.registrate.GTRegistrate;
+import com.mo_guang.ctpp.api.StressRecipeCapability;
 import com.mo_guang.ctpp.common.data.CTPPRecipeCapabilities;
 import com.mo_guang.ctpp.recipe.CTPPRecipes;
+import com.mojang.datafixers.util.Pair;
 import net.minecraft.data.recipes.FinishedRecipe;
 
 import java.util.function.Consumer;
+
+import static com.mo_guang.ctpp.integration.kjs.CTPPRecipeComponents.SU_IN;
+import static com.mo_guang.ctpp.integration.kjs.CTPPRecipeComponents.SU_OUT;
 
 @GTAddon
 public class CTPPGTAddon implements IGTAddon {
@@ -34,5 +40,13 @@ public class CTPPGTAddon implements IGTAddon {
     @Override
     public void addRecipes(Consumer<FinishedRecipe> provider) {
         CTPPRecipes.init(provider);
+    }
+
+    @Override
+    public void registerRecipeKeys(KJSRecipeKeyEvent event) {
+        event.registerKey(
+                StressRecipeCapability.CAP,
+                Pair.of(SU_IN, SU_OUT)
+        );
     }
 }
