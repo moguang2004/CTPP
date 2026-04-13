8e5c785bb147bab34f84658ad82780d7a1f330cc
lucky_block <1774432258@qq.com>
2026-04-14 01:12:11 +0800

    fix: revert change

--- src/main/java/com/mo_guang/ctpp/CTPPGTAddon.java
+++ src/main/java/com/mo_guang/ctpp/CTPPGTAddon.java
@@ -3,11 +3,13 @@ package com.mo_guang.ctpp;
 import com.gregtechceu.gtceu.api.addon.GTAddon;
 import com.gregtechceu.gtceu.api.addon.IGTAddon;
 import com.gregtechceu.gtceu.api.addon.events.KJSRecipeKeyEvent;
+import com.gregtechceu.gtceu.api.machine.multiblock.MultiblockPreviewHighlightRegistry;
 import com.gregtechceu.gtceu.api.registry.registrate.GTRegistrate;
 
 import net.minecraft.data.recipes.FinishedRecipe;
 import net.minecraft.resources.ResourceLocation;
 
+import com.mo_guang.ctpp.api.CTPPPartAbility;
 import com.mo_guang.ctpp.api.CTPPRecipeCapabilities;
 import com.mo_guang.ctpp.api.StressRecipeCapability;
 import com.mo_guang.ctpp.api.pattern.CTPPBlockMaps;
@@ -40,15 +42,15 @@ public class CTPPGTAddon implements IGTAddon {
         return CTPP.MODID;
     }
 
-    // @Override
-    // public void registerMultiblockPreviewHighlighters(MultiblockPreviewHighlightRegistry registry) {
-    // registry.registerAbilityHighlight(MultiblockPreviewHighlightRegistry.INPUT_COLOR,
-    // CTPPPartAbility.INPUT_KINETIC);
-    // registry.registerAbilityHighlight(MultiblockPreviewHighlightRegistry.OUTPUT_COLOR,
-    // CTPPPartAbility.OUTPUT_KINETIC);
-    // registry.registerAbilityHighlight(MultiblockPreviewHighlightRegistry.MAINTENANCE_COLOR,
-    // CTPPPartAbility.MECHANICAL_UPGRADE);
-    // }
+    @Override
+    public void registerMultiblockPreviewHighlighters(MultiblockPreviewHighlightRegistry registry) {
+        registry.registerAbilityHighlight(MultiblockPreviewHighlightRegistry.INPUT_COLOR,
+                CTPPPartAbility.INPUT_KINETIC);
+        registry.registerAbilityHighlight(MultiblockPreviewHighlightRegistry.OUTPUT_COLOR,
+                CTPPPartAbility.OUTPUT_KINETIC);
+        registry.registerAbilityHighlight(MultiblockPreviewHighlightRegistry.MAINTENANCE_COLOR,
+                CTPPPartAbility.MECHANICAL_UPGRADE);
+    }
 
     @Override
     public void registerRecipeCapabilities() {
