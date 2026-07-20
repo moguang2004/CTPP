b8d270657df837551b2660bdb0c06af0ced07945
mmyddd <98249707+mmyddd@users.noreply.github.com>
2026-07-21 00:48:08 +0800

    feat: 删掉三峡大坝思索中的仓室位置限制提示

--- src/main/java/com/mo_guang/ctpp/client/ponder/kinetic/BigDam.java
+++ src/main/java/com/mo_guang/ctpp/client/ponder/kinetic/BigDam.java
@@ -100,8 +100,6 @@ public class BigDam {
 
         scene.idle(20);
 
-        scene.overlay().showOutline(PonderPalette.RED, "left", ioPortLeft, 40);
-        scene.overlay().showOutline(PonderPalette.RED, "right", ioPortRight, 40);
         scene.showText(40,
                 "You can place interfaces at the location of the stone bricks, such as input bins, stress output bins, etc",
                 "你可以在石砖的位置放置接口，如输入仓，应力输出仓等")
